f847bb09d90a572771afb4505691ee203367d574
daniellutziger <daniel.lutziger@hispeed.ch>
2020-06-06 22:48:11 +0200

    changed data

--- src/main/java/com/infom/daniellutziger/repositories/ManufacturerRepository.java
+++ src/main/java/com/infom/daniellutziger/repositories/ManufacturerRepository.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long> {
 
-    @Query(value="SELECT DISTINCT m from Manufacturer m join m.singleOrder odr where ")
-    public List<String> asf();
+    //@Query(value="SELECT DISTINCT m from Manufacturer m join m.singleOrder odr where ")
+    //public List<String> asf();
 
     @Query(value="SELECT DISTINCT c.name FROM Company c JOIN Man")
     public List<String> getCompanyNames();
