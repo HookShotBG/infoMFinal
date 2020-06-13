ec149fb90f6f887b0fe69c55d5d12534a2cdb49e
daniellutziger <daniel.lutziger@hispeed.ch>
2020-06-13 18:06:29 +0200

    everything except xml

--- src/main/java/com/infom/daniellutziger/rest/CompanyRestController.java
+++ src/main/java/com/infom/daniellutziger/rest/CompanyRestController.java
@@ -3,6 +3,8 @@ package com.infom.daniellutziger.rest;
 import com.infom.daniellutziger.entities.Company;
 import com.infom.daniellutziger.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +22,18 @@ public class CompanyRestController {
     private CompanyRepository cr;
 
     @GetMapping("/byDate/{date}")
-    public List<String> getCompanyNames(@PathVariable String date){
+    public ResponseEntity<List<String>> getCompanyNames(@PathVariable String date){
         //prefixed date
         //String str="2018-03-31";
         //http://localhost:8080/company/byDate/2020-03-31
         Date generatedDate =Date.valueOf(date);
         List<String> names = cr.getCompanyNameByManufacturerByOrderByOrderDate(generatedDate);
-        return names;
+
+        if (names != null && !names.isEmpty()) {
+            return new ResponseEntity<List<String>>(names, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<List<String>>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("")
